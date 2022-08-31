7cfb63f563fbdb7c14fbb2c25b52af2f8b3678c7
freyj7 <freyj7@bfh.ch>
2022-08-31 13:55:15 +0200

    updated error message

--- O_data_manager.module.js
+++ O_data_manager.module.js
@@ -3,7 +3,7 @@ import * as log from "https://deno.land/std@0.153.0/log/mod.ts";
 
 class O_data_manager{
     constructor(){
-        log.error("this module is not working anymore, please use the module 'o_json_db', found on 'https://deno.land/x/o_json_db");
+        log.error("O_data_manager.module.js: this module is not working anymore, please use the module 'o_json_db', found on 'https://deno.land/x/o_json_db");
     }
 }
 
