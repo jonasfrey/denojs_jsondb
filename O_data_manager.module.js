43d4bf0dd1c118e3673b663129891126e09122aa
freyj7 <freyj7@bfh.ch>
2022-08-31 13:43:29 +0200

    moving from deno.lan/x/jsondb to deno.land/x/O_json_db2

---
+++ O_data_manager.module.js
@@ -0,0 +1,10 @@
+
+import * as log from "https://deno.land/std@0.153.0/log/mod.ts";
+
+class O_data_manager{
+    constructor(){
+        log.error("this module is not working anymore, please use the module 'o_json_db', found on 'https://deno.land/x/o_json_db");
+    }
+}
+
+export {O_data_manager}
